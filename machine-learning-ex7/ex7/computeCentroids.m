8141bed5e11c9ebc79aaf29ccfc069b7fa060029
chongkong <chongkong94@gmail.com>
2016-10-23 03:20:52 +0900

    Implemented k-means

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -16,25 +16,10 @@ function centroids = computeCentroids(X, idx, K)
 % You need to return the following variables correctly.
 centroids = zeros(K, n);
 
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Go over every centroid and compute mean of all points that
-%               belong to it. Concretely, the row vector centroids(i, :)
-%               should contain the mean of the data points assigned to
-%               centroid i.
-%
-% Note: You can use a for-loop over the centroids to compute this.
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
+for i = 1:K
+    cluster_points = X(idx == i, :);
+    centroids(i, :) = sum(cluster_points) / size(cluster_points, 1);
+end
 
 end
 
